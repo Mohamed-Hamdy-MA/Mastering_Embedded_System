8ed49d0c5b5fb7dfe355eb85b6aa3914085133ac
Mohamed Hamdy <mohamedhamdy.m.h.m.a@gmail.com>
2024-06-26 12:07:40 +0300

    Embedded_C_Lesson_1_Homework

---
+++ Unit_3_Embedded_C/Lesson_1/Creating Static Library/can.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "can.h"
+
+
+void can_init()
+{
+	printf("%s\n","can_init >>>>>>> " );
+}
---
+++ Unit_3_Embedded_C/Lesson_1/Platform_Types.h
@@ -0,0 +1,18 @@
+#ifndef _Platform_Types_H_
+#define _Platform_Types_H_
+
+#include <stdio.h>
+
+#typedef unsigned char 				boolen;
+
+#typedef signed char 				uint8;
+#typedef unsigned char 				sint8;
+#typedef signed short 				sint16;
+#typedef unsigned short 			uint16;
+#typedef signed int 				sint32;
+#typedef unsigned int 				uint32;
+#typedef signed long long 			sint64;
+#typedef signed long long 			uint64;
+
+#typedef float						float32;
+#typedef double						float64;
